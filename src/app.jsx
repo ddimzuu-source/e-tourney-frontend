879f96fbaa35f7040b98939a224b7a4e9362227d
drmwnmass <ddimzuu@gmail.com>
2026-06-26 22:08:50 +0700

    fix css

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 //import './bootstrap';
+import './app.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
